8e191cecfcd8b95a0b81a0d3d8a514be0126f766
VladVonKhym <kalessin3@hotmail.fr>
2020-04-15 16:11:35 +0200

    IA_4_001 - classe Arbre
    Ajout de la classe Arbre et Maillon

---
+++ Gaufre/src/ai/Tree.java
@@ -0,0 +1,36 @@
+package ai;
+
+import ai.Node;
+
+import java.util.ArrayList;
+
+public class Tree<E> {
+	
+	private Node<E> root;
+	
+	public Tree(E element) {
+		root = new Node<E>(element);
+	}
+	
+	/* Cette fonction prend un élement E elm et 
+	 * renvoie toutes les occurences de elm dans l'arbre */
+	public ArrayList<Node<E>> occurence(E element){
+		return occurence(element, root);
+	}
+	
+	private ArrayList<Node<E>> occurence(E element, Node<E> n){
+		ArrayList<Node<E>> current = new ArrayList<Node<E>>();
+		
+		if(n.getElement() == element) current.add(n);
+		
+		for(int i = 0; i < current.size() ; i++) current.addAll(occurence(element, (Node<E>) n.nodes().get(i)));
+		
+		return current;
+	}
+	
+	public Node<E> root(){
+		return root;
+	}
+	
+	
+}
